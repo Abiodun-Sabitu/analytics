ba97703c3aa0688426c6ea6a8a42e5cd36b95adf
abiodun.sabitu <abiodun.sabitu@sterling.ng>
2024-01-24 10:43:36 +0100

    setup react project: install vite and tailwind plugin

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function App() {
+  return (
+    <>
+      <h1 className=" text-blue-500 font-bold underline text-2xl">
+        Hello World!
+      </h1>
+    </>
+  );
+}
+
+export default App;
