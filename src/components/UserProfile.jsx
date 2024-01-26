bc57a0f406a6b21061be6edf4e6e2f71bdc8ff90
Aduwo Ayooluwa <aduwoayooluwa@gmail.com>
2024-01-26 23:02:32 +0100

    _

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -8,38 +8,42 @@ import {
   MenuList,
   Button,
   VStack,
+  useColorMode
 } from "@chakra-ui/react";
 import { ChevronDownIcon } from "@chakra-ui/icons";
 import Settings_icon from "../assets/Settings_icon.svg";
 import logout_icon from "../assets/logout_icon.svg";
 
-const userProfile = () => {
+const UserProfile = () => {
+  const { colorMode } = useColorMode();
+  
   return (
-    <Menu bg="white" border borderRadius="100px" zIndex="10" h="52px">
+    <Menu bg={colorMode === "dark" ? "rgba(0, 0, 0, 0.40)" : "white"} border borderRadius="100px" zIndex="10" h="52px">
       <MenuButton
         as={Button}
         borderColor="#DADDDD"
         borderWidth={"1px"}
         borderRadius={"100px"}
         h="52px"
-        bg="white"
+        bg={colorMode === "dark" ? "rgba(0, 0, 0, 0.40)" : "white"} 
+        _hover={colorMode === "dark" ? "bg-black" : "gray.50"}
         rightIcon={<ChevronDownIcon boxSize={7} color="#4F4D57" />}
         spacing={0}
       >
         <HStack>
           <Avatar src="/img/profile_img.png" size="sm" />
           <VStack>
-            <Text fontSize="16px" fontWeight={400} textColor="#26282C">
+            <Text fontSize="16px" fontWeight={400} textColor={colorMode === "dark" ? "#E5EAEF" : "#26282C"}>
               Justin Bergson
             </Text>
-            <Text textColor={"#787486"} fontWeight={400} fontSize={"14px"}>
+            <Text textColor={colorMode === "dark" ? "gray.300" : "#787486"} fontWeight={400} fontSize={"14px"}>
               Justin@gmail.com
             </Text>
           </VStack>
         </HStack>
       </MenuButton>
 
-      <MenuList>
+      <MenuList bg={colorMode === "dark" ? "rgba(0, 0, 0, 0.40)" : "white"}>
         <MenuItem>
           <img src={Settings_icon} alt="settings" className="mr-3" /> Settings
         </MenuItem>
@@ -51,4 +55,4 @@ const userProfile = () => {
   );
 };
 
-export default userProfile;
+export default UserProfile;
